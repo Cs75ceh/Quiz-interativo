9316475ed5263bf3dd19da2a9ff752ad34587a13
Cs75ceh <67435914+Cs75ceh@users.noreply.github.com>
2022-04-26 16:10:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-/*
-JS file
-*/
+const form = document.querySelector('.quiz-form')
+
+const correctAnswers = [4, 1, 4, 4, 1,2]
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    userAnswers = [
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value,
+        form.inputQuestion5.value,
+        form.inputQuestion6.value
+    ]
+    
+    let score = 0
+    const increment = 100 / 6 
+    userAnswers.forEach((userAnswer, index) => {
+        console.log(userAnswer, correctAnswers[index])
+        
+        if (Number(userAnswer) === correctAnswers[index]) {
+            score = score + increment 
+        }
+    });
+    console.log('Score : ', score)
+})
